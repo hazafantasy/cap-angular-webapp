a267e88884c1364dd818973a26a3387104d759b2
haza <mojica.hazael@gmail.com>
2017-03-11 13:38:50 -0600

    abemus datatables

--- angular2FrontEnd/src/client/app/dashboard/cap-invoices/cap-invoices.component.ts
+++ angular2FrontEnd/src/client/app/dashboard/cap-invoices/cap-invoices.component.ts
@@ -1,35 +1,58 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
 
 @Component({
     moduleId: module.id,
     selector: 'cap-invoices',
-    templateUrl: './cap-invoices.component.html',
-    styleUrls:['https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css']
+    templateUrl: './cap-invoices.component.html'
 })
 export class CapInvoicesComponent {
-    theTitle = {val : 'Here are all the invoices'};
-    invoices = [
+    public data: any;
+    public filterQuery = "";
+    public rowsOnPage = 10;
+    public sortBy = "email";
+    public sortOrder = "asc";
+    
+    constructor(private http:Http){
+
+    }
+
+    ngOnInit(): void {
+        this.http.get("app/dashboard/cap-invoices/data.json")
+            .subscribe((data)=> {
+                setTimeout(()=> {
+                    this.data = data.json();
+                }, 1000);
+            });
+    }
+
+    public toInt(num: string) {
+        return +num;
+    }
+
+    public sortByWordLength = (a: any) => {
+        return a.city.length;
+    }
+
+    public invoices = [
         {
-            id: '1',
-            order_date: '21/04/1990',
-            user_name: 'John Doe',
-            total: '$999.99'
+            "id": "1",
+            "order_date": "21/04/1990",
+            "user_name": "John Doe",
+            "total": "$999.99"
         },
         {
-            id: '2',
-            order_date: '22/04/2000',
-            user_name: 'Aragorn',
-            total: '$333.33'
+            "id": "2",
+            "order_date": "22/04/2000",
+            "user_name": "Aragorn",
+            "total": "$333.33"
         },
         {
-            id: '3',
-            order_date: '23/04/2017',
-            user_name: 'Andraste',
-            total: '$111.11'
+            "id": "3",
+            "order_date": "23/04/2017",
+            "user_name": "Andraste",
+            "total": "$111.11"
         }
     ];
-    
-    ngOnInit() {
-        
-    }
+
 }
--- angular2FrontEnd/src/client/app/dashboard/cap-invoices/cap-invoices.module.ts
+++ angular2FrontEnd/src/client/app/dashboard/cap-invoices/cap-invoices.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CapInvoicesComponent } from './cap-invoices.component';
+import { DataTableModule } from 'angular2-datatable';
+import { HttpModule } from '@angular/http';
+import { DataFilterPipe }   from './cap-invoices-data-filter.pipe';
 
 @NgModule({
-    imports: [FormsModule, CommonModule],
-    declarations: [CapInvoicesComponent],
+    imports: [FormsModule, CommonModule, DataTableModule, HttpModule],
+    declarations: [CapInvoicesComponent, DataFilterPipe],
     exports: [CapInvoicesComponent]
 })
 
