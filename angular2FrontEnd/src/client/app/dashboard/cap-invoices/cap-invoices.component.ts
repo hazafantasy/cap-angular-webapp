2bb27431ea88eebf08b879c86487a63f70921eda
haza <mojica.hazael@gmail.com>
2017-03-11 18:50:07 -0600

    Stupid angular who cannot play nice with JQuery Datatables

--- angular2FrontEnd/src/client/app/dashboard/cap-invoices/cap-invoices.component.ts
+++ angular2FrontEnd/src/client/app/dashboard/cap-invoices/cap-invoices.component.ts
@@ -34,25 +34,4 @@ export class CapInvoicesComponent {
         return a.city.length;
     }
 
-    public invoices = [
-        {
-            "id": "1",
-            "order_date": "21/04/1990",
-            "user_name": "John Doe",
-            "total": "$999.99"
-        },
-        {
-            "id": "2",
-            "order_date": "22/04/2000",
-            "user_name": "Aragorn",
-            "total": "$333.33"
-        },
-        {
-            "id": "3",
-            "order_date": "23/04/2017",
-            "user_name": "Andraste",
-            "total": "$111.11"
-        }
-    ];
-
 }
